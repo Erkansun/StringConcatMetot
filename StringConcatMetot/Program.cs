2fa1fa03925a0856802b3f5aa7af2870b12dc093
egune <egune@ERKANSUNPC>
2025-05-09 13:09:29 +0300

    Add project files.

---
+++ StringConcatMetot/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace StringConcatMetot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string TCno = "12345678901";
+            string ad = "Ali";
+            string soyad = "Veli";
+            double maas = 2500;
+            int cocukSayisi = 2;
+
+            string birlestirilenVeri = string.Concat(TCno + " " + ad + " " + soyad + " " + maas + " " + cocukSayisi);
+
+            string birlestirilenVeri2 = string.Concat($"TC No: {TCno}, Adı: {ad}, Soyadı: {soyad}, Maaş: {maas}, Çocuk Sayısı: {cocukSayisi}");
+
+
+            Console.WriteLine(birlestirilenVeri);
+
+            Console.WriteLine();
+
+            Console.WriteLine(birlestirilenVeri2);
+
+            Console.ReadKey();
+        }
+    }
+}
